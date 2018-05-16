a789fdcaa095524f7aa15ce5d3fcd5f4254df50e
kamalkrishna3000 <kamalkrishna3000@gmail.com>
2018-05-16 09:22:32 +0530

    Projects Created.

---
+++ React Native/trainingkit1/android/app/src/main/java/com/trainingkit1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trainingkit1;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "trainingkit1";
+    }
+}
